e03c2c014f38935398bf187c0d6bc907d0974144
JulieAllix <allixjulie@gmail.com>
2020-06-10 15:25:03 +0200

    Constants created

---
+++ constants/defaultStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../constants/themeColors';
+
+export default StyleSheet.create({
+    headerText: {
+        fontFamily: 'lobster',
+        color: Colors.light,
+        fontSize: 28,
+    }
+});
