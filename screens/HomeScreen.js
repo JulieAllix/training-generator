b2415076a17cf6b42418bd4195ee2a03c2fb3866
JulieAllix <allixjulie@gmail.com>
2020-06-10 16:21:18 +0200

    Import HomeScreen in App

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 import Colors from '../constants/themeColors';
 
 const HomeScreen = props => {
-    
+    return (
+        <View style={styles.screen}>
+          <Text>Coucou</Text>
+        </View>
+      );
 };
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
 });
 
 export default HomeScreen;
