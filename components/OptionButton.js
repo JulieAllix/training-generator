456c070c3e0f60ee09a97819f8068cdada33c232
JulieAllix <allixjulie@gmail.com>
2020-06-15 18:15:21 +0200

    Responsive design of Equipment screen done

--- components/OptionButton.js
+++ components/OptionButton.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TouchableOpacity, 
+    Image,
+    Dimensions
+} from 'react-native';
 
 import Colors from '../constants/themeColors';
 import DefaultStyles from '../constants/defaultStyles';
 
 const OptionButton = props => {
+    const [availableDeviceWidth, setAvailableDeviceWidth] = useState(Dimensions.get('window').width);
+    const [availableDeviceHeight, setAvailableDeviceHeight] = useState(Dimensions.get('window').height);
+
+    const [buttonWidth, setButtonWidth] = useState(availableDeviceWidth * 0.4);
+    const [buttonHeight, setButtonHeight] = useState(availableDeviceWidth * 0.4);
+
+    useEffect(() => {
+        const updateLayout = () => {
+            setAvailableDeviceWidth(Dimensions.get('window').width);
+            setAvailableDeviceHeight(Dimensions.get('window').height);
+        };
+
+        Dimensions.addEventListener('change', updateLayout);
+
+        return () => {
+            Dimensions.removeEventListener('change', updateLayout);
+        };
+    });
+
+    useEffect(() => {
+        const updateButton = () => {
+            setButtonWidth(availableDeviceWidth * 0.4);
+            setButtonHeight(availableDeviceWidth * 0.4);
+        };
+    
+        Dimensions.addEventListener('change', updateButton);
+        
+        return () => {
+            Dimensions.removeEventListener('change', updateButton);
+        };
+    });
+
     return (
-        <TouchableOpacity style={styles.option}>
+        <TouchableOpacity style={{...styles.option, height: buttonHeight, width: buttonWidth }}>
             <Image 
                 style={styles.image} 
                 source={props.src}
@@ -22,8 +61,8 @@ const OptionButton = props => {
 
 const styles = StyleSheet.create({
     option: {
-        width: 150,
-        height: 150,
+        //width: Dimensions.get('window').width > 350 ? 150 : 140,
+        //height: Dimensions.get('window').width > 350 ? 150 : 140,
         borderWidth: 1,
         borderColor: Colors.accent,
         padding: 10,
@@ -32,7 +71,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     image: { 
-        height: '65%',
+        height: '60%',
         marginVertical: 13,
     },
     
